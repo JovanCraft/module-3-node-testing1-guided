543d13fc75b19e805aa09f0a4b6c7d278b4b8960
jovan <jovancraft@gmail.com>
2024-02-20 14:32:43 -0600

    created a test to test if the car had a make and model property as well as implimented the make and model in the Car class

--- car/car.spec.js
+++ car/car.spec.js
@@ -1,3 +1,5 @@
+const Car = require('./car')
+
 function foo () {
     return 'the FOO'
 }
@@ -24,6 +26,18 @@ describe('our first tests', () => {
 describe('foo function', () => {
     test('foo returns the FOO', () => {
         expect(foo()).toBe('the FOO')
+        expect(foo()).toHaveLength(7)
+    })
+})
+describe('Car class', () => {
+    test('it is defined', () => {
+        expect(Car).toBeDefined()
+        expect(Car).toBeInstanceOf(Function) /* *** */
+    })
+    test('has model and make', () => {
+        const prius = new Car('toyota', 'prius')
+        expect(prius).toHaveProperty('make')
+        expect(prius).toHaveProperty('model')
     })
 })
 //all of the expect cases have a value of 5 so for that, the test is passing. If even ONE of them had the wrong value in the toBe area, it would fail.
@@ -31,3 +45,4 @@ describe('foo function', () => {
 //If i were to say expect(o).toBe(oo) this would fail because I've though they LOOK the same, they are pointing to 2 different objects in memory!!
 //BUT expect(oo).toBe(ooo) is TRUE! because ooo is pointing to oo in memory(SEE FIRST COMMENT!)
 //on SECOND COMMENT toEqual passes this test becuase these objects EQUAL the same thing, but toBe would fail because are in two different points of memory, therefore NOT THE SAME THING
+//on THIRD COMMENT, this passing because car is a class/constructor and ALL constructors are FUnctions at the end of the day!
